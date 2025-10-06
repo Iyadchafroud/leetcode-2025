78ecea41a8383c87f40b1e388377acca9b93e7aa
mohamed iyed chafroud <60520088+Iyadchafroud@users.noreply.github.com>
2025-10-06 22:32:50 +0100

    Time: 242 ms (33.36%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+    -- Write your PostgreSQL query statement below.
+      select a.salary
+    from (select e.salary, dense_rank() over (order by e.salary desc) r from Employee e) a
+    where a.r = n
+    limit 1
+      
+  );
+END;
+$$ LANGUAGE plpgsql;
