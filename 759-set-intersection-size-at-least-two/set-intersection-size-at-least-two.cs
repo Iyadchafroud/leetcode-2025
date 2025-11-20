1fdebed6c6f13d543364a7e60cd337c5c0ab929d
mohamed iyed chafroud <60520088+Iyadchafroud@users.noreply.github.com>
2025-11-20 21:38:45 +0100

    Time: 5 ms (100.00%) | Memory: 49 MB (33.33%) - LeetSync

---
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.cs
@@ -0,0 +1,34 @@
+ class Solution {
+    public int IntersectionSizeTwo(int[][] intervals) {
+        Array.Sort(intervals, (a, b) => {
+            if (a[1] != b[1]) return a[1].CompareTo(b[1]);
+            return b[0].CompareTo(a[0]);
+        });
+
+        int count = 0;
+        int a = -1, b = -1;  // a = largest chosen, b = second largest
+
+        foreach (var inr in intervals) {
+            int start = inr[0], end = inr[1];
+
+            bool aIn = (a >= start && a <= end);
+            bool bIn = (b >= start && b <= end);
+
+            if (aIn && bIn) {
+                continue;
+            }
+
+            if (aIn) {
+                b = a;
+                a = end;
+                count++;
+            } else {
+                b = end - 1;
+                a = end;
+                count += 2;
+            }
+        }
+
+        return count;
+    }
+}
